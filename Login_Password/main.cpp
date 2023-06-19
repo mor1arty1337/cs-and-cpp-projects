0cc3d2acce55576faf382492379f41c0ccb54cee
Danil <90785942+mor1arty1337@users.noreply.github.com>
2023-06-19 17:57:51 +0300

    Registration
    A simple program for registration (with the creation of a file where the login and password are stored)

---
+++ Login_Password/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+bool IsRegistered()
+{
+    string username, password, un, pw;
+    cout << "Enter username: " << endl;
+    cin >> username;
+    cout << "Enter password: " << endl;
+    cin >> password;
+    ifstream read("d:\\Coding\\C++\\Login_Password\\AllUsers" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+    if (un == username && pw == password)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int choise;
+    string username, password;
+    cout << "Enter 1 to register" << endl;
+    cout << "Enter 2 to login" << endl;
+    cout << "Your choise:";
+    cin >> choise;
+    if (choise == 1)
+    {
+        cout << "Create username:" << endl;
+        cin >> username;
+        cout << "Create password:" << endl;
+        cin >> password;
+        ofstream file;
+        file.open("d:\\Coding\\C++\\Login_Password\\AllUsers" + username + ".txt");
+        file << username << endl;
+        file << password << endl;
+        file.close();
+    }
+    else if (choise == 2)
+    {
+        bool status = IsRegistered();
+        if (status == false)
+        {
+            cout << "False login" << endl;
+            system("PAUSE");
+            return 0;
+        }
+        else
+        {
+            cout << "You successfully logged in" << endl;
+            system("PAUSE");
+            return 1;
+        }
+    }
+}
