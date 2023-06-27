d3600c9689b4219448225b21f6fcce9416a1dd85
Danil <90785942+mor1arty1337@users.noreply.github.com>
2023-06-27 14:48:02 +0300

    Update main.cpp
    Now "user" can delete his account and change password

--- Login_Password/main.cpp
+++ Login_Password/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdio>
 using namespace std;
 
 bool IsRegistered()
@@ -10,6 +11,7 @@ bool IsRegistered()
     cin >> username;
     cout << "Enter password: " << endl;
     cin >> password;
+
     ifstream read("d:\\Coding\\C++\\Login_Password\\AllUsers\\User_" + username + ".txt");
     getline(read, un);
     getline(read, pw);
@@ -19,12 +21,62 @@ bool IsRegistered()
         return false;
 }
 
+void DeleteAccount()
+{
+    string username, password, un, pw;
+    cout << "Enter username: " << endl;
+    cin >> username;
+    cout << "Enter password: " << endl;
+    cin >> password;
+    ifstream read("d:\\Coding\\C++\\Login_Password\\AllUsers\\User_" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+    read.close();
+    if (un == username && pw == password)
+    {
+        string path = "d:\\Coding\\C++\\Login_Password\\AllUsers\\User_" + username + ".txt";
+        remove(path.c_str());
+        cout << path << endl;
+        cout << "This account has been successfully deleted" << endl;
+    }
+    else
+        cout << "You wrote something wrong" << endl;
+}
+
+void PasswordChange()
+{
+    cout << "To confirm changes enter your data again" << endl;
+    string username, oldpassword, newpassword, un, pw;
+    cout << "Confirm username:" << endl;
+    cin >> username;
+    cout << "Confirm password:" << endl;
+    cin >> oldpassword;
+    ifstream read("d:\\Coding\\C++\\Login_Password\\AllUsers\\User_" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+    if (un == username && pw == oldpassword)
+    {
+        cout << "Enter your new password" << endl;
+        cin >> newpassword;
+        ofstream file;
+        file.open("d:\\Coding\\C++\\Login_Password\\AllUsers\\User_" + username + ".txt");
+        file.clear();
+        file << username << endl;
+        file << newpassword << endl;
+        file.close();
+        cout << "New password set" << endl;
+    }
+    else
+        cout << "Something went wrong" << endl;
+}
 int main()
 {
     int choise;
+    bool loggedin;
     string username, password;
     cout << "Enter 1 to register" << endl;
     cout << "Enter 2 to login" << endl;
+    cout << "Enter 3 to delete account" << endl;
     cout << "Your choise:";
     cin >> choise;
     if (choise == 1)
@@ -46,14 +98,23 @@ int main()
         if (status == false)
         {
             cout << "False login" << endl;
-            system("PAUSE");
-            return 0;
         }
         else
         {
             cout << "You successfully logged in" << endl;
-            system("PAUSE");
-            return 1;
+            loggedin = true;
         }
     }
+    else if (choise == 3)
+        DeleteAccount();
+    if (loggedin == true)
+    {
+        printf("Enter 1 to change your password\n");
+        cin >> choise;
+        if (choise == 1)
+        {
+            PasswordChange();
+        }
+    }
+    return 1;
 }
